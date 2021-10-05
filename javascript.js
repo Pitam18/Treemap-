38c01437647def32171a0aa0d354e6ae1e2d9060
Pitam Kumar Dey <85692326+Pitam18@users.noreply.github.com>
2021-10-05 13:29:25 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var toggler = document.getElementsByClassName("first");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("first-down");
+  });
+}
+
